6e5210c6dd4440526ed68bd6d1d9f7086a37e43e
Aljomai <cgpiao1@up.edu.ph>
2017-05-27 06:46:05 +0800

    Push 1

--- database.js
+++ database.js
@@ -1,2 +1,35 @@
-var pg = require('pg');
+const Sequelize = require('sequelize');
 
+const connectionUrl = 'postgres://pgdemo:pgdemo@localhost:5432/pgdemo';
+const database = new Sequelize(connectionUrl);
+
+const Visitor = database.define('visitors', {
+	id: {
+		type: Sequelize.INTEGER,
+		primaryKey: true,
+		autoIncrement: true,
+		allowNull: false
+	},
+	fname: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+    lname: {
+        type: Sequelize.STRING,
+		allowNull: false
+    },
+	country: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	message: {
+		type: Sequelize.STRING,
+		allowNull: false
+	}
+}, {
+	timestamps: true
+});
+
+database.sync();
+
+module.exports.Visitor = Visitor;
--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var consolidate = require('consolidate');
+const cookieparser = require('cookie-parser');
+const flash = require('express-flash');
+var bodyparser = require('body-parser');
+var Visitor = require('./database').Visitor;
+const session = require('express-session');
+
 
 var app = express();
 
@@ -12,6 +18,10 @@ app.use(express.static('./Logos'));
 app.use(express.static('./resources'));
 app.use(express.static('./BackgroundPicture'));
 
+app.use(bodyparser.urlencoded());
+
+app.use(session({ resave: false, saveUninitialized: false, secret: 'secret-cookie' }));
+app.use(flash());
 
 app.get('/',function(req,res){
 	res.render('index.html');
@@ -27,6 +37,25 @@ app.get('/about',function(req,res){
 
 app.get('/contact',function(req,res){
 	res.render('contact.html');
+});
+
+app.post('/contacts', function(req,res){
+	console.log(req.body);
+
+	var fname = req.body.firstname;
+	var lname = req.body.lastname;
+	var country = req.body.country;
+	var message = req.body.message;
+
+	Visitor.create({
+            fname: fname,
+            lname: lname,
+            country: country,
+            message: message
+    }).then(function(){
+        req.flash('statusMsg', 'Successfully sent message!');
+    	res.redirect('/contact');
+    });
 });
 
 app.get('/resume',function(req,res){
